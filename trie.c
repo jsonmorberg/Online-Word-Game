c7edafd135ec9565fa23c4a1d9dcfa425f4b811a
morberj2 <morberj2@wwu.edu>
2020-10-27 05:19:13 +0000

    Revert "trie.c"
    This reverts commit 7e931dcb6908059db8ace31bd9f25a7de1b9ded3

--- trie.c
+++ trie.c
@@ -1,1 +1,79 @@
 //SOURCE https://www.geeksforgeeks.org/trie-insert-and-search/
+//TODO change bool types to int 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "trie.h"
+/*
+struct TrieNode 
+{ 
+    struct TrieNode *children[ALPHABET_SIZE]; 
+  
+    // isEndOfWord is true if the node represents 
+    // end of a word 
+    bool isEndOfWord; 
+}; 
+ */ 
+// Returns new trie node (initialized to NULLs) 
+struct TrieNode *getNode(void) 
+{ 
+    struct TrieNode *pNode = NULL; 
+  
+    pNode = (struct TrieNode *)malloc(sizeof(struct TrieNode)); 
+  
+    if (pNode) 
+    { 
+        int i; 
+  
+        pNode->isEndOfWord = false; 
+  
+        for (i = 0; i < ALPHABET_SIZE; i++) 
+            pNode->children[i] = NULL; 
+    } 
+  
+    return pNode; 
+}
+
+void insert(struct TrieNode *root, const char *key)
+{
+    int level;
+    int length = strlen(key);
+    int index;
+
+    struct TrieNode *pCrawl = root;
+
+    for (level = 0; level < length; level++)
+    {
+        index = CHAR_TO_INDEX(key[level]);
+        if (!pCrawl->children[index]){
+            pCrawl->children[index] = getNode();
+        }
+        pCrawl = pCrawl->children[index];
+    }
+
+    // mark last node as leaf
+    pCrawl->isEndOfWord = true;
+}
+
+// Returns true if key presents in trie, else false
+bool search(struct TrieNode *root, const char *key)
+{
+    int level;
+    int length = strlen(key);
+    int index;
+    struct TrieNode *pCrawl = root;
+
+    for (level = 0; level < length; level++)
+    {
+        index = CHAR_TO_INDEX(key[level]);
+
+        if (!pCrawl->children[index])
+            return false;
+
+        pCrawl = pCrawl->children[index];
+    }
+
+    return (pCrawl != NULL && pCrawl->isEndOfWord);
+} 
+
