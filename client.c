a88e8806c4c1f891b22e221cf0dcfd3f24d37fa0
Cameron McCoy Moon <moonc2@linux-01.cs.wwu.edu>
2020-11-01 14:21:40 -0800

    network start

--- client.c
+++ client.c
@@ -18,7 +18,12 @@ int main( int argc, char **argv) {
 	char *host; /* pointer to host name */
 	int n; /* number of characters read */
 	char buf[1000]; /* buffer for data from the server */
-
+    uint8_t boardSize;
+    uint8_t seconds;
+    uint8_t player;
+    uint8_t round;
+    uint8_t score;
+    uint8_t opScore;
 	memset((char *)&sad,0,sizeof(sad)); /* clear sockaddr structure */
 	sad.sin_family = AF_INET; /* set family to Internet */
 
@@ -68,13 +73,22 @@ int main( int argc, char **argv) {
 		fprintf(stderr,"connect failed\n");
 		exit(EXIT_FAILURE);
 	}
-
-	n = recv(sd, buf, sizeof(buf), 0);
-	while (n > 0) {
-		write(1,buf,n);
-		n = recv(sd, buf, sizeof(buf), 0);
-	}
-
+    recv(sd,&player,sizeof(uint8_t),0);
+    recv(sd,&boardSize,sizeof(uint8_t),0);
+    recv(sd,&seconds,sizeof(uint8_t),0);
+    printf("%d, %d, %d\n", player,  boardSize, seconds);
+    score = 69;
+    //GAME START
+    recv(sd, &round, sizeof(uint8_t), 0);
+    printf("starting round %d\n", round);
+    send(sd, &score, sizeof(uint8_t), 0);
+    recv(sd, &opScore, sizeof(uint8_t), 0);
+    printf("my = %d op = %d\n",score, opScore);
+    char board[boardSize];
+    recv(sd, board, boardSize,0);
+    board[boardSize] = '\0';
+    printf("%s\n", board);
+    //recive board
 	close(sd);
 	exit(EXIT_SUCCESS);
 }
